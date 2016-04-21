adb40555bac1fe00b0aada73169305c58a0e3a35
GtrCraft <nickvanbokhorst@hotmail.com>
2016-04-21 10:05:04 +0200

    Add Krexus_maintainer.mk

---
+++ krexus_maintainer.mk
@@ -0,0 +1,4 @@
+#Current Krexus Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.krexus.maintainer="Nick_van_Bokhorst" \
+    ro.krexus.maintainer.info="Also_known_as_GtrCraft_-_Maintainer_for_Grouper_and_Lux"
